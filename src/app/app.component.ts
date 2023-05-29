45e0647d24b3da98069f583637b3c8000155738a
AnandhaKumar S <anandhakumarnathan@gmail.com>
2023-05-29 14:19:33 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myapp';
+  menuItems: {
+    [key: string]: { description: string; name: string; price: number };
+  } = {
+    m1: {
+      description: 'Chicken Fried Rice',
+      name: 'FriedRice',
+      price: 150.0,
+    },
+    m2: {
+      description: 'Mutton Briyani',
+      name: 'Briyani',
+      price: 175.0,
+    },
+    m3: {
+      description: 'Idly with Vada',
+      name: 'Idly',
+      price: 50.0,
+    },
+    m4: {
+      description: 'Dosai with potato masala',
+      name: 'Dosai',
+      price: 55.0,
+    },
+  };
+
+  get menuItemsArray() {
+    return Object.keys(this.menuItems).map(
+      (key: string) => this.menuItems[key]
+    );
+  }
 }
